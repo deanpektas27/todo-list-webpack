582646e34ab28484914758b08acc9fd4553e0c66
Dean Pektas <deanpektas12345@gmail.com>
2023-08-08 18:29:28 -0400

    Created function that returns one reminder with required parameters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './style.css';
+import { format } from 'date-fns'
 
-console.log('googoo gaga!');
+
+const content = document.querySelector('#content');
+
+// "Todos will be objects that you'll DYNAMICALLY CREATE,
+// using either factories OR constructors/classes"
+
+// Lets start with creating a factory that creates reminders,
+// then log them to console (make this a separate module)
+
+// A reminder has a title, description, dueDate and priority
+// Will add more parameters later
+const reminderFactory = (title, description, priority) => {
+    return {
+        title, 
+        description, 
+        priority
+    };
+};
+
+const reminder = reminderFactory('Get milk', 'Go to Stop n Shop for milk', 'low');
+
+content.innerText = `${reminder.title}\n ${reminder.description}\n ${reminder.priority} Priority`;
+
+// Next, lets add an array that pushes the reminders to it
