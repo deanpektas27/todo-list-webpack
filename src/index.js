aab1f891accf09dbda8116d44d2215465b9aa5c5
Dean Pektas <deanpektas12345@gmail.com>
2023-08-08 21:07:44 -0400

    Modified todo list structure to push data into array of objects

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ const content = document.querySelector('#content');
 // "Todos will be objects that you'll DYNAMICALLY CREATE,
 // using either factories OR constructors/classes"
 
-// Lets start with creating a factory that creates reminders,
-// then log them to console (make this a separate module)
-
 // A reminder has a title, description, dueDate and priority
 // Will add more parameters later
 const todoFactory = (title, description, priority) => {
@@ -20,7 +17,6 @@ const todoFactory = (title, description, priority) => {
     };
 };
 
-// Next, lets add an array that pushes the reminders to it
 let projectLists = {
     default: [
         {
@@ -40,29 +36,31 @@ const render = () => {
     content.innerHTML = '';
 }
 
-// Lets now add all todos in the project list to the screen
 const displayTodos = (list) => {
     render();
-    // create a function that creates DOM elements with todos
     createTodoDOMElements(list);
 }
 
 const createTodoDOMElements = (list) => {
-    // Create DIV for every new reminder for easy styling
     for(const key in list) {
-        console.log(list[key]);
+        console.table(list[key].priority);
+        // div container for each reminder
         let div = document.createElement('div');
-        div.setAttribute('id', `data-index-${key}`)
+        div.setAttribute('id', `data-index-${key}`);
+        // title for each reminder
         let h3 = document.createElement('h3');
         h3.innerText = list[key].title;
+        // description for each reminder
+        let p = document.createElement('p');
+        p.innerText = list[key].description;
+
         div.appendChild(h3);
+        div.appendChild(p);
         content.appendChild(div);
     }
 }
 
 // Push data to list so it may be used by console/DOM
-// Object.assign(projectLists.default, todoFactory('Get milk', 'Go to Stop n Shop for milk', 'low'));
-// Object.assign(projectLists.default, todoFactory('Car bill!!', 'payment is past due..', 'high'));
-
-//console.table(projectLists);
+projectLists.default.push(todoFactory('SWE Interview w/ Abraham', 'Recruiter from linkedin', 'high'));
+projectLists.default.push(todoFactory('Lunch with Kevin', 'Moho (he owes me)', 'medium'));
 displayTodos(projectLists.default);
