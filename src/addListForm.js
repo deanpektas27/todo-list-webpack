41a875ab576039bc4bc50b56c4e7f3657b4d76fa
Dean Pektas <deanpektas12345@gmail.com>
2023-08-09 22:16:40 -0400

    Added functionality to list form, can now add new lists

--- src/addListForm.js
+++ src/addListForm.js
@@ -1,7 +1,38 @@
+import { addList } from ".";
 
+const addListForm = (entireList) => {
+    let form = document.createElement('form');
+    let h2 = document.createElement('h2');
+    h2.innerText = 'Add New List';
+    h2.setAttribute('style', 'text-decoration: underline');
+    form.setAttribute('style', `margin: 20px;border: 2px solid black;padding: 10px;`);
+    form.setAttribute('id', `addList`);
+    form.appendChild(h2);
 
-const addListForm = () => {
+    // Input Title box
+    let reminderTitleLabel = document.createElement('label');
+    reminderTitleLabel.setAttribute('for','listTitle');
+    reminderTitleLabel.innerText = 'Title';
+    let reminderTitleInput = document.createElement('input');
+    reminderTitleInput.setAttribute('type','text');
+    reminderTitleInput.setAttribute('id','listTitle');
+    form.appendChild(reminderTitleLabel);
+    form.appendChild(reminderTitleInput);
 
+    // Submit List Btn
+    let submitList = document.createElement('input');
+    submitList.setAttribute('type','button');
+    submitList.setAttribute('value','Add List');
+    submitList.setAttribute('id','submitList');
+    form.appendChild(submitList);
+
+    content.appendChild(form);
+    
+    submitList.addEventListener('click', () => {
+        let title = document.querySelector('#listTitle').value;
+        console.log(title);
+        addList({[title]: []});
+    })
 }
 
 export { addListForm };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { format } from 'date-fns'
 import { createTodoDOMElements } from './createTodoDOMElements';
 import { projectLists } from './projectLists';
 import { addReminderForm } from './addReminderForm';
+import { addListForm } from './addListForm';
 
 const content = document.querySelector('#content');
 
@@ -11,6 +12,7 @@ const content = document.querySelector('#content');
 const render = () => {
     content.innerHTML = '';
     addReminderForm(projectLists);
+    addListForm(projectLists);
 
     createTodoDOMElements(projectLists);
 }
@@ -33,16 +35,13 @@ const addReminderToList = (list, title, description, priority) => {
 
 const addList = (name) => {
     Object.assign(projectLists, name);
+    render();
 }
 
 
 // Push data to list so it may be used by console/DOM
 //addReminderToList('school', 'SWE Interview w/ Abraham', 'Recruiter from linkedin', 'high');
 
-// Checking if addList function works
-addList({'Wish': []});
-addReminderToList('Wish', 'playstation 5', 'need this!!', 'high');
-
 render();
 
-export { addReminderToList };
+export { addReminderToList, addList };
